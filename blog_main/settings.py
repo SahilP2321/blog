d977695113eb867cac2b111f282b3aaf39049aa6
Sahil <patilsahil.23p@gmail.com>
2026-03-19 17:35:48 +0530

    Blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'blog_main/static',
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
