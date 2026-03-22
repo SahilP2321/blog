3be47944370a0d40dfe75824a2e18d0629200e55
Sahil <patilsahil.23p@gmail.com>
2026-03-22 21:54:11 +0530

    added comments functionality

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category ,Blog ,About , Sociallinks
+from .models import Category ,Blog ,About , Sociallinks , Comment
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
@@ -20,3 +20,4 @@ admin.site.register(Category)
 admin.site.register(Blog , BlogAdmin)
 admin.site.register(About,AboutAdmin)
 admin.site.register(Sociallinks)
+admin.site.register(Comment)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from blogs.models import Category, Blog 
+from blogs.models import Category, Blog ,Comment
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -15,7 +16,16 @@ def posts_by_category(request, category_id):
 
 def blogs(request,slug):
     single_blog = get_object_or_404(Blog,slug=slug,status='Published')
+    if request.method == "POST":
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(blog=single_blog)
     context = {
+        "comments":comments,
         "single_blog":single_blog,
     }
     return render(request , 'blogs.html',context)
